068ab11f75ad1821100c85b9acf4107f6a66a447
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-16 15:30:02 -0400

    acommit

--- Fibinocci-part1/main.c
+++ Fibinocci-part1/main.c
@@ -8,8 +8,67 @@
 
 #include <stdio.h>
 
+int recursivevalueFibinochi(int remainingstep, int previousvalue, int previouspreviousvalue);
+int recursiverefrenceFibinochi(int remainingstep, int *previousvalue, int *previouspreviousvalue);
+int Fibinocci(int n);
+//
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int answer= Fibinocci(10);
+    printf("the value of result is %d\n",answer);
+    
+    
+    int answer2 = recursivevalueFibinochi(10, 0, 1);
+    printf("the value of result2 is %d\n",answer2);
+    
+    int previousvalue= 0;
+    int previouspreviousvalue= 1;
+    
+    int answer3 = recursiverefrenceFibinochi(10, &previousvalue, &previouspreviousvalue);
+    printf("the value for result 3 is %d\n",answer3);
     return 0;
 }
+
+int Fibinocci(int n){
+    if (n <= 0){
+        return 0;
+    }
+
+    int result= 0;
+    int previousresult= 0;
+    int doublepreviousresult= 1;
+    
+    for (int fibnum = 0; fibnum < n; fibnum++){
+        printf("test %i\n", result);
+        result = previousresult + doublepreviousresult;
+        doublepreviousresult = previousresult;
+         previousresult = result;
+    }
+    return  result;
+}
+
+// the second part of the question
+int recursivevalueFibinochi(int remainingstep, int previousvalue, int previouspreviousvalue){
+    if (remainingstep < 1){
+        return  previousvalue;
+    }
+    else
+       return recursivevalueFibinochi(remainingstep -1,previousvalue + previouspreviousvalue,previousvalue);
+}
+
+
+// the third part of the question
+int recursiverefrenceFibinochi(int remainingstep, int *previousvalue, int *previouspreviousvalue){
+    
+    if (remainingstep < 1){
+        return  *previousvalue;
+    }
+    
+    int swap = *previousvalue + *previouspreviousvalue ;
+    *previouspreviousvalue = *previousvalue ;
+    *previousvalue = swap;
+    
+    
+    return recursiverefrenceFibinochi(remainingstep -1,previousvalue,previouspreviousvalue);
+}
+
